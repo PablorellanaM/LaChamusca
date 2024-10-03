84eac673cb3c17c8d830e3a4c9b7b6dd4cf2fe34
PablorellanaM <ore20555@uvg.edu.gt>
2024-10-02 21:42:50 -0600

    Update Navigation.kt

--- app/src/main/java/com/example/lachamusca/navigation/Navigation.kt
+++ app/src/main/java/com/example/lachamusca/navigation/Navigation.kt
@@ -6,12 +6,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.lachamusca.view.CrearPartidoScreen
-import com.example.lachamusca.view.EncontrarPartidoScreen
-import com.example.lachamusca.view.LoginScreen
-import com.example.lachamusca.view.MatchListScreen
-import com.example.lachamusca.view.MenuScreen
-import com.example.lachamusca.view.ProfileScreen
+import com.example.lachamusca.view.*
 
 @Composable
 fun AppNavigation(modifier: Modifier = Modifier) {
@@ -31,6 +26,12 @@ fun AppNavigation(modifier: Modifier = Modifier) {
         composable("createMatch") {
             CrearPartidoScreen(navController)  // Pantalla para crear partidos
         }
+        composable("popularMatches") {
+            PartidosPopularesScreen(navController)  // Pantalla de partidos populares
+        }
+        composable("teamsToJoin") {
+            EquiposUnirteScreen(navController)  // Pantalla de equipos a los que puedes unirte
+        }
         composable("menu") {
             MenuScreen(navController = navController)  // Pantalla del menú
         }
@@ -40,11 +41,18 @@ fun AppNavigation(modifier: Modifier = Modifier) {
     }
 }
 
-
 fun ProfileScreen(navController: NavHostController) {
     // Implementación de la pantalla de perfil
 }
 
 fun CrearPartidoScreen(navController: NavHostController) {
     // Implementación de la pantalla para crear partidos
+}
+
+fun PartidosPopularesScreen(navController: NavHostController) {
+    // Implementación de la pantalla de partidos populares
+}
+
+fun EquiposUnirteScreen(navController: NavHostController) {
+    // Implementación de la pantalla para equipos a los que puedes unirte
 }
