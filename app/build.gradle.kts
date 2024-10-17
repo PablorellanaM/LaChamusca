cbb679dfb405c20bc5ee6f0d9def1ef53313c241
PablorellanaM <ore20555@uvg.edu.gt>
2024-10-17 11:03:25 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,12 +32,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11 // Cambiar a Java 11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11" // Cambiar a Java 11
     }
 
     buildFeatures {
